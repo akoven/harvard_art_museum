a823ab1fdc14c6e0dfe3c32be401b6ce5a08f757
Attiya <adarensburg@live.com>
2022-06-14 18:43:09 -0500

    galleries are listed

---
+++ art-museum/src/components/GalleryNavigation/index.js
@@ -0,0 +1,23 @@
+import { NavLink } from "react-router-dom";
+
+const GalleryNavigation = ({galleries}) => {
+    console.log('galleries: ',galleries);
+    return(
+        <nav>
+            <NavLink to='/'>
+                <h1>Galleries</h1>
+            </NavLink>
+            <ul>
+                {galleries.map(gallery => {
+                    return(
+                        <nav>
+                            <NavLink key={gallery.id} to={`/galleries/${gallery.id}`}>{gallery.name}</NavLink>
+                        </nav>
+                    )
+                })}
+            </ul>
+        </nav>
+    )
+}
+
+export default GalleryNavigation;
--- art-museum/src/index.js
+++ art-museum/src/index.js
@@ -1,11 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+// import { createRoot } from 'react-dom/client';
+
+import {BrowserRouter} from 'react-router-dom';
 import './index.css';
 import App from './App';
 
+const Root = () => {
+  return(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
   document.getElementById('root')
 );
+
+
+
+
+// const container = document.getElementById('root');
+// const root = createRoot(container);
+// root.render(
+//   <React.StrictMode>
+//     <Root />
+//   </React.StrictMode>
+//   )
+export default Root;
